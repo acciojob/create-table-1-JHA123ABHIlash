6d227025105987360e07eec1a6439c5a811d3f6e
Abhilash Jha <152012438+JHA123ABHIlash@users.noreply.github.com>
2025-11-14 22:13:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+
 function insert_Row() {
     //Write your code here
+	let row=document.createElement("tr");
+
+	
+	let cell1=document.createElement("td");
+	cell1.textContent="New Cell1";
+	row.appendChild(cell1);
+
+	
+	let cell2=document.createElement("td");
+	cell2.textContent="New Cell2";
+	row.appendChild(cell2);
   
-  
+  document.querySelector("#sampleTable").prepend(row);
 }
